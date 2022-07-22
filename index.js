0d5aae08443c7601e5d7ec9dd2df5895fda8df25
sam nk lcf <samyeyebe@gmail.com>
2022-07-23 00:42:52 +0100

    samuel

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('static'))
+app.set('views', './views');
+app.set('view engine', 'ejs')
+
+
+
 app.get('/', (req, res) => {
-    res.send("samuel");
+    res.render('index.ejs')
 });
 
-app.listen("3000", () => {
-    console.log('http://localhost:3000');
+app.listen("3333", () => {
+    console.log('http://localhost:3333');
 })
